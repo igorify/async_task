809da4b14bcbf362d7a234189290b60deeb23f2e
Igor <igor@iMac-Admin.local>
2016-12-06 11:41:29 -0800

    added configs for NODE_ENV

--- app.js
+++ app.js
@@ -1,54 +1,52 @@
 const express = require('express');
 const app = express();
 const request = require('request');
+const config = require('config');
 
 app.listen(3000, ()=> console.log('App start listening on post 3000'));
 
-//console.log('NODE_ENV:', process.env.NODE_ENV);
+console.log('NODE_ENV:', process.env.NODE_ENV);
 
 const statuses = {
     UP: 'UP',
     DOWN: 'DOWN'
 };
 
+let resources = config.get('Resources');
+
+
+let listObj = {};
+
+
+function wrapRequest(resourseName, cb){
+    request(resourseName, (error, response) => {
+        console.log(response.statusCode);
+
+        if(error) return res.status(500).send('Error');
+
+        /*if( setTimeout( response.statusCode == undefined, 3000)) {
+         listObj[resourseName] = statuses.DOWN
+         } else */if (response.statusCode <=302) {
+            listObj[resourseName] = statuses.UP;
+        } else {
+            listObj[resourseName] = statuses.DOWN;
+        }
+        cb();
+    });
+}
+
 app.get('/health_check',  (req, res) => {
 
-    let resources = {
-        google: 'https://www.google.com.ua',
-        youtube: 'https://www.youtube.com/dsfkdds',
-        yandex: 'https://www.yanddddex.com/'
-    };
-
-    let listObj = {};
-
-
-    function wrapRequest(resourseName, cb){
-        request(resourseName, (error, response) => {
-            //console.log(response.statusCode);
-
-            if(error) return res.status(500).send('Error');
-
-            /*if( setTimeout( response.statusCode == undefined, 3000)) {
-                listObj[resourseName] = statuses.DOWN
-            } else */if (response.statusCode <=302) {
-                listObj[resourseName] = statuses.UP;
-            } else {
-                listObj[resourseName] = statuses.DOWN;
-            }
-            cb();
-        });
-    }
-
-        google();
+        uber();
 
         mergedJson = () => {res.send(JSON.stringify(listObj))};
 
-        function google() {
-            wrapRequest(resources.google, youtube);
+        function uber() {
+            wrapRequest(resources.uber, spotify);
         }
 
-        function youtube() {
-            wrapRequest(resources.youtube, mergedJson);
+        function spotify() {
+            wrapRequest(resources.spotify, mergedJson);
         }
 
       /*  function yandex() {
