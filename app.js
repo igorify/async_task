982ecd0c956711ef40a8f84c755634bf13b35295
Igor <igor@iMac-Admin.local>
2016-12-07 13:47:57 +0200

    .

--- app.js
+++ app.js
@@ -14,36 +14,39 @@ const statuses = {
 };
 
 let resources = config.get('Resources');
+let listObj = {};
 
+function wrapRequest(resourseName, cb){
 
-function wrapRequest(resourseName, listObj, cb){
-    //t.aboad() cancelRequet Method
-    let r =request(resourseName, /*{timeout: 3000},*/ (error, response) => {
+    let timeout = setTimeout(()=>{
+        //r.abourt();
+        listObj[resourseName] = statuses.DOWN;
+    }, 3000);
+
+    let r = request(resourseName, (error, response) => {
 
         if(error) console.error(error.stack);
-  /*      if(setTimeout(()=> {
-            }, 3000);
-*/
-        if (setTimeout(()=> !response, 3000)){
+
+       /* if (setTimeout(()=> !response, 3000)){
             listObj[resourseName] = statuses.DOWN;
-        } else if (response.statusCode <=302) {
+        } else */if (response.statusCode <=302) {
             listObj[resourseName] = statuses.UP;
         } else {
             listObj[resourseName] = statuses.DOWN;
-        }
-        cb();
+        } console.log(listObj);
+
+        clearTimeout(timeout);
     });
+    cb();
 }
 
 app.get('/health_check',  (req, res) => {
 
-    let listObj = {};
     let operations = 0;
     let resourcesLength = Object.keys(resources).length;
 
     Object.keys(resources).forEach((resource)=> {
-        wrapRequest(resources[resource], listObj, (operations) => operations++);
-
+        wrapRequest(resources[resource], ()=> operations++);
         if (operations == resourcesLength) return res.send(JSON.stringify(listObj))
 
     });
